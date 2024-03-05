733950a50a7a5ad3f11f95ac441d8df9e3bfd5e2
Paulo Ribeiro <plribeiro3000@gmail.com>
2024-03-05 06:41:57 -0300

    chore(*): Initial Commit

---
+++ lib/validates_identity/pe_ruc.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pe_ruc/version"
+
+module ValidatesIdentity
+  module PeRuc
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
